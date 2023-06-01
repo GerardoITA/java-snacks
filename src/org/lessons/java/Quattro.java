6ad0bcf7096e3f77d36c7f9c153be28dc65cc73a
GerardoITA <gerardo.giusti@tiscali.it>
2023-06-01 16:41:24 +0200

    snack 4

--- src/org/lessons/java/Quattro.java
+++ src/org/lessons/java/Quattro.java
@@ -1,11 +1,28 @@
 package org.lessons.java;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Quattro {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci una parola");
+        String parola = scanner.next();
 
+        char[] parolaArray = parola.toCharArray();
+        char[] parolaInversa = new char[parolaArray.length];
 
+        int count = 0;
+        for (int i = parolaArray.length -1 ; i >= 0; i-- ) {
+            parolaInversa[count] = parolaArray[i];
+            count++;
+        }
+
+        if (Arrays.equals(parolaInversa, parolaArray)) {
+            System.out.println("Palindroma");
+        } else {
+            System.out.println("Non palindroma");
+        }
 
 
     }
