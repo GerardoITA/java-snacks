a694aa1189c33f62a6e21617c7bc7c05e132a201
GerardoITA <gerardo.giusti@tiscali.it>
2023-06-01 15:02:25 +0200

    snack 9

--- src/org/lessons/java/Nove.java
+++ src/org/lessons/java/Nove.java
@@ -1,8 +1,22 @@
 package org.lessons.java;
-import java.util.Arrays;
 import java.util.Scanner;
 public class Nove {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int quantiNumeri = 10;
+
+        double somma = 0;
+        double media = 0;
+        for (int i = 0; i < quantiNumeri; i++) {
+            System.out.println("Inserisci un numero intero");
+            double numero = scanner.nextDouble();
+
+            somma += numero;
+
+        }
+        media = somma / quantiNumeri;
+
+        System.out.println("La somma è " + somma + " mentre la media è " + media );
 
     }
 }
